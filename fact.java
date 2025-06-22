c589f32c59c30891c81fd3a706db01d18ddb579f
VINNARASAN S <vinnarasans.23aid@kongu.edu>
2025-06-22 13:39:53 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,16 @@
+class Main{
+    static int fact(int n)
+    {
+        if(n==0) return -1;
+        else{
+             return n*fact(n-1);
+        }
+    }
+    public static void main(String[] args)
+    {
+      int n=10;
+     
+      System.out.println(fact(n));
+      
+    }
+}
